c7507fda8dcf9da6efc363568970e6c996c51159
unknown <cookieKang@gmail.com>
2019-08-07 00:34:47 +0900

    line-through 처리

--- sample_todo/src/Todo/TodoList.js
+++ sample_todo/src/Todo/TodoList.js
@@ -5,10 +5,10 @@ const TodoList = ({todos, change}) => {
 
     const list = todos.map( ({tno, title, complete}) => {
 
-
+        const style = complete ? {textDecorationLine: 'line-through', textDecorationStyle: 'solid'} : {}
 
         return (
-            <li key={tno}>
+            <li key={tno} style={style}>
                 {title} 
                 <input type='checkbox' checked = {complete ?'checked':''} onChange={() => change(tno)} ></input> </li>
         )
